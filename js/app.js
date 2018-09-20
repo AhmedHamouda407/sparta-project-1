1cca4310fcabc7b09678a02e4ca4c2dbe0528726
Tech-A23 <tech-a23@Tech-A23s-MacBook-Air.local>
2018-09-20 15:38:51 +0100

    majority of game logic working

--- js/app.js
+++ js/app.js
@@ -12,11 +12,19 @@ document.addEventListener('DOMContentLoaded', function(){
   // testing random words
 
   console.log('working')
-  var wordarray = ['january', 'january', 'january'];
+  var wordarray = ['march', 'march', 'march'];
   var randomword = wordarray[Math.floor(Math.random() * wordarray.length)]
   var guessarray =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  var randomstoredword = [];
+  randomstoredword.push(randomword)
+  var randomswordletters = randomword.split("")
+  var randomlettersstored= [];
+  randomlettersstored.push(randomswordletters)
 
+  console.log(randomstoredword);
   console.log(randomword);
+  console.log(randomswordletters);
+  console.log(randomlettersstored);
 
   // generate random word
   //determine number of characters in word
@@ -29,6 +37,7 @@ document.addEventListener('DOMContentLoaded', function(){
   //add list items
   for (var i = 0; i < randomword.length; i++ ){
     const li=document.createElement('li');
+
     li.innerHTML= randomword[i];
     ul.appendChild(li);
   }
@@ -37,13 +46,60 @@ document.addEventListener('DOMContentLoaded', function(){
 //     console.log(input);
 //
 // console.log('working');
-if (randomword[0]==='j'){
-  console.log('true')
-  document.getElementById('list').style.color = 'red';//and append the first child of the list to change to red
-  document.getElementById("list").firstChild.innerHTML
+// if (randomword[0]==='j' ){//&&randomword[0]=user input)
+//   console.log('true')
+//   document.getElementById('list').style.color = 'red';//and append the first child of the list to change to red
+//   document.getElementById("list").children[0].style.color = 'red';
+//
+// }if (randomword[1]==='a' && randomword[4]==='a'){
+//   console.log('true')
+//   document.getElementById('list').style.color = 'red';//and append the first child of the list to change to red
+//   document.getElementById("list").children[1].style.color = 'red';
+//   document.getElementById("list").children[4].style.color = 'red';
+//
+// }
+function changecolor (){
+  if (randomword[0]==='j' ){//&&randomword[0]=user input)
+    console.log('true')
+    document.getElementById('list').style.color = 'red';//and append the first child of the list to change to red
+    document.getElementById("list").children[0].style.color = 'red';
+
+  }if (randomword[1]==='a' && randomword[4]==='a'){
+    console.log('true')
+    document.getElementById('list').style.color = 'red';//and append the first child of the list to change to red
+    document.getElementById("list").children[1].style.color = 'red';
+    document.getElementById("list").children[4].style.color = 'red';
+
+  }
 
 }
 
+// changecolor();
 
+      // document.getElementById("guess").addEventListener("click", function(){
+      //     console.log('test');
+      //
+      // });
+      document.getElementById("hangman").addEventListener("submit", function(e){
+        e.preventDefault();
+        for(let i=0; i<randomlettersstored.length; i++){
+          let value = document.getElementById("letter").value;
+          console.log("letter value:", value)
+          if(randomlettersstored[i].includes(value)){
+            console.log("yaaay boiii");
+            let colourKid = randomlettersstored[i].indexOf(value);
+            console.log(colourKid)
+            document.getElementById("list").children[colourKid].style.color = 'red';
+          } else {
+            console.log("awwww")
+          }
+        }
+
+
+      });
+      document.getElementById("restart").addEventListener("click", function(e){
+          console.log('restart');
+
+      });
 
 })
