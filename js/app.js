923718d10e677b823b19c98d65775ae32f2dce99
Tech-A23 <tech-a23@Tech-A23s-MacBook-Air.local>
2018-09-21 11:16:54 +0100

    final changes

--- js/app.js
+++ js/app.js
@@ -1,64 +1,49 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-
-
-var wordarray = ['linux', 'ctf', 'ram', 'hack', 'virus'];
-var randomword = wordarray[Math.floor(Math.random() * wordarray.length)]
-var guessarray =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var randomstoredword = [];
-randomstoredword.push(randomword)
-var randomswordletters = randomword.split("")
-var randomlettersstored= [];
-var score = 5
-document.getElementById("scoreCounter").innerHTML = score
-randomlettersstored.push(randomswordletters)
-
-
-var userinputstored=[];
-var userinput=document.getElementById("letter")
-userinputstored.push(userinput.value)
-
-
-
-
-const ul = document.getElementById('list')
-
-
-for (var i = 0; i < randomword.length; i++ ){
-  const li=document.createElement('li');
-
-  li.innerHTML= randomword[i];
-  ul.appendChild(li);
-}
-
-let guessedWord = '';
-document.getElementById("hangman").addEventListener("submit", function(e){
-  e.preventDefault();
-  for(let i=0; i<randomlettersstored.length; i++){
-    let value = document.getElementById("letter").value;
-
-    if(randomlettersstored[i].includes(value)){
-      guessedWord += value;
-
-      let colourKid = randomlettersstored[i].indexOf(value);
-
-      document.getElementById("list").children[colourKid].style.color = 'red';
-      if (guessedWord === randomword) {
-        alert('well done, you win')
-      }
-    } if (!randomlettersstored[i].includes(value)) {
-      alert('wrong letter, try again'),
-      score--
-      document.getElementById("scoreCounter").innerHTML = score
-    } else {
-
-    }
-  }if (score===0){
-    alert('unlucky, start another game')
+  var wordarray = ['linux', 'ctf', 'ram', 'hack', 'virus'];
+  var randomword = wordarray[Math.floor(Math.random() * wordarray.length)]
+  var guessarray =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  var randomstoredword = [];
+  randomstoredword.push(randomword)
+  var randomswordletters = randomword.split("")
+  var randomlettersstored= [];
+  var score = 5
+  document.getElementById("scoreCounter").innerHTML = score
+  randomlettersstored.push(randomswordletters)
+  var userinputstored=[];
+  var userinput=document.getElementById("letter")
+  userinputstored.push(userinput.value)
+  var ul = document.getElementById('list')
+  
+  for (var i = 0; i < randomword.length; i++ ){
+    const li=document.createElement('li');
+    li.innerHTML= randomword[i];
+    ul.appendChild(li);
   }
-});
 
-document.getElementById("restart").addEventListener("click", function(e){
+  let guessedWord = '';
+  document.getElementById("hangman").addEventListener("submit", function(e){
+    e.preventDefault();
+    for(let i=0; i<randomlettersstored.length; i++){
+      let value = document.getElementById("letter").value;
 
-});
+      if(randomlettersstored[i].includes(value)){
+        guessedWord += value;
+        let colourKid = randomlettersstored[i].indexOf(value);
+
+        document.getElementById("list").children[colourKid].style.color = 'red';
+        if (guessedWord === randomword) {
+          alert('well done, you win')
+        }
+      }
+      if (!randomlettersstored[i].includes(value)) {
+        alert('wrong letter, try again'),
+        score--
+        document.getElementById("scoreCounter").innerHTML = score
+      }
+    }
+    if (score===0){
+      alert('unlucky, start another game')
+    }
+  });
 })
